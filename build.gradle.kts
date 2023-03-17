50d7713f08a3eb3874234eb5524e442d3ea2ecd7
Anmol Verma <anmol.verma4@gmail.com>
2023-03-17 19:06:57 +0100

    setup :ci-cd

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,3 +23,9 @@ tasks.register("publishToMavenLocal") {
     //No need to publish the plugin itself
     dependsOn(":grpc-multiplatform-lib:publishToMavenLocal")
 }
+
+// On Apple Silicon we need Node.js 16.0.0
+// https://youtrack.jetbrains.com/issue/KT-49109
+rootProject.plugins.withType(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin::class) {
+    rootProject.the(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension::class).nodeVersion = "16.0.0"
+}
